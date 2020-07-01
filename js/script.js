5a601b170e7537bfcaf3fbae78738b08a33aa795
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-07-01 22:01:04 +0200

    Adding Javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 document.getElementById("playAudio").addEventListener("click", function(){
     var audio = document.getElementById('testAudio');
+    var barra = document.getElementById('barra');
   if(this.className == 'is-playing'){
     this.className = "";
+    barra.classList.remove("playing")
     this.innerHTML = "<i class='far fa-play-circle'></i>"
     audio.pause();
   }else{
     this.className = "is-playing";
+    barra.classList.add("playing");
     this.innerHTML = "<i class='far fa-pause-circle'></i>";
     audio.play();
   }
